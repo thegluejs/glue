5759eecceebf1619b755557e9e89d2aec6035f26
Fábio Luciano <fabio@naoimporta.com>
2017-01-30 16:17:53 -0200

    Adding more thing

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
   var glue = require('./lib/glue');
 
   glue
-    .namespace('namespace', 'Glob all that things')
-    .namespace('xxx')
-    .namespace('xxx');
+    .namespace('build', 'Glob all that build things')
+      .namespace('application', 'Glob all that application things')
+        .namespace('other', 'Glob all that other things')
+          .task('teste', function() {
+            console.log('asdsdsa');
+          });
 
   // glue.task();
   //
